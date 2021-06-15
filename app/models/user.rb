b7933ce107e8510bfb0c59606971cfefcc7eab31
Lehoax <leolair.pro@gmail.com>
2021-06-15 05:40:17 -0700

    fix view bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  after_create :welcome_mail
+  #after_create :welcome_mail
   
   def welcome_mail
     UserMailer.welcome(self).deliver
