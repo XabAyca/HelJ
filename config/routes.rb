26952be2a5a2227b3fe7afbd2c20dff8dfc9b675
jocelin queau <queau.j@gmail.com>
2021-06-15 11:47:19 +0200

    show page for challenge

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get "challenges/index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :challenges, only: [:index, :show]
 end
