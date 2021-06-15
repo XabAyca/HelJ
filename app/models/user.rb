6c574a70bb0a95b505b1ad54bbcd14af69199b17
Xabi <xabi.aycaguer@gmail.com>
2021-06-15 14:30:03 +0200

    add project to user relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :joint_users_to_projects
+  has_many :projects, through: :joint_users_to_projects
+  has_many :projects, foreign_key: 'owner_id', class_name: 'Project', dependent: :destroy
+  
 end
