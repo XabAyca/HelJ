9cc27a8dd926cff6ccbd0f8e03097dd468076727
Xabi <xabi.aycaguer@gmail.com>
2021-06-16 09:57:09 +0200

    add create & destroy method to project participation

--- app/controllers/joint_users_to_projects_controller.rb
+++ app/controllers/joint_users_to_projects_controller.rb
@@ -3,15 +3,16 @@ class JointUsersToProjectsController < ApplicationController
   before_action :max_participations?, only: [:create]
 
   def create 
-    @participation = JointUsersToProject.create(project_id:params[:id],user_id:current_user.id)
-    redirect_to project_path(params[:id]),notice:'Vous êtes inscrit sur le projet, un email vous a été envoyé'
+    @participation = JointUsersToProject.create(project_id:params[:project_id],user_id:current_user.id)
+    redirect_to project_path(params[:project_id]),notice:'Vous êtes inscrit sur le projet, un email vous a été envoyé'
   end
 
   def destroy
-    @project_participations = JointUsersToProject.where(project_id:params[:id])
+    @project_participations = JointUsersToProject.where(project_id:params[:project_id])
+    p @project_participations
     @project_participations.filter{
       |participation| participation.user_id == current_user.id
     }[0].delete
-    redirect_to project_path(params[:id]), notice:'Vous n\'êtes plus inscrit sur le projet'
+    redirect_to project_path(params[:project_id]), notice:'Vous n\'êtes plus inscrit sur le projet'
   end
 end
--- app/helpers/joint_users_to_projects_helper.rb
+++ app/helpers/joint_users_to_projects_helper.rb
@@ -1,9 +1,9 @@
 module JointUsersToProjectsHelper
 
   def max_participations? 
-    @participations = JointUsersToProject.where(project_id:params[:id])
+    @participations = JointUsersToProject.where(project_id:params[:project_id])
     if @participations.count >= 3
-      redirect_to project_path(params[:id]), notice:'Maximum de participants atteint' 
+      redirect_to project_path(params[:project_id]), notice:'Maximum de participants atteint' 
     end
   end
 end
