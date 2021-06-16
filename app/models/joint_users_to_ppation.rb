c2a8243a289de92b1710159be1b30a60ff70f9e7
Lehoax <leolair.pro@gmail.com>
2021-06-16 05:35:56 -0700

    make mailer function

--- app/models/joint_users_to_ppation.rb
+++ app/models/joint_users_to_ppation.rb
@@ -1,4 +1,9 @@
 class JointUsersToPpation < ApplicationRecord
   belongs_to :user
   belongs_to :participation_challenge
+    after_create :welcome_mail
+
+    def user_joint_challenge
+      UserMailer.user_on_challenge(self).deliver
+    end
 end
