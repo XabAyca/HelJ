c7d544374a7c8903bd85aa5d4f79dea50715f5a0
Xabi <xabi.aycaguer@gmail.com>
2021-06-21 21:35:23 +0200

    add admin user dashboard

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,5 +2,15 @@ class Admin::UsersController < ApplicationController
   before_action :is_admin?
   before_action :authenticate_user!
 
+  def index
+    @users = User.all
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_path, notice:'Utilisateur supprimé'
+  end
+
 
 end
