976ac444af710903e7c9c01656e07c00fd5a7a2c
jocelin queau <queau.j@gmail.com>
2021-06-22 23:35:34 +0200

    conflict

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,20 +1,9 @@
 //= require chartkick
 //= require Chart.bundle
 //= require navbar
-<<<<<<< HEAD
-//= require jquery
-//= require jquery_ujs
-//= require_tree .
-//= require turbolinks
-||||||| 6d0a862
-//= require jquery
-//= require jquery_ujs
-//= require_tree .
-=======
 //= require turbolinks
 
 
 //= require_tree .
 
 
->>>>>>> actioncable_lasttry
