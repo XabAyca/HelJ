b03cb7adf6a850b1fea3b1570458e0ec71883306
jocelin queau <queau.j@gmail.com>
2021-06-23 01:02:08 +0200

    basic messaging working

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,26 @@ class MessagesController < ApplicationController
   def create
     @message = Message.new(content: params[:content], room_id: params[:room_id], user_id: current_user.id)
     if @message.save
-      redirect_to request.referrer
+      ActionCable.server.broadcast "room_channel_#{@message.room_id}", html: render_message
     end
+  end
+
+  def render_message
+    MessagesController.renderer.instance_variable_set(
+      :@env, {
+        "HTTP_HOST" => "localhost:3000",
+        "HTTPS" => "off",
+        "REQUEST_METHOD" => "GET",
+        "SCRIPT_NAME" => "",
+        "warden" => warden,
+      }
+    )
+
+    MessagesController.render(
+      partial: "messagerie/message",
+      locals: {
+        message: @message,
+      },
+    )
   end
 end
--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,16 +1,25 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create("RoomChannel", {
-  connected() {
-	  console.log('hello')
-    // Called when the subscription is ready for use on the server
-  },
+document.addEventListener('turbolinks:load',()=>{
+	const roomElement = document.querySelector("#room-id")
+	const roomId = Number(roomElement.getAttribute('data-room-id'))
+	consumer.subscriptions.create({ channel:"RoomChannel",room_id:roomId}, {
+		connected() {
+			console.log(`connected to ${roomId}`)
+		  // Called when the subscription is ready for use on the server
+		},
+	  
+		disconnected() {
+		  // Called when the subscription has been terminated by the server
+		},
+	  
+		received(data) {
+			console.log(data)
+			const messagesContainer = document.querySelector('#messages')
+			messagesContainer.innerHTML = messagesContainer.innerHTML + data.html
+			console.log('send')
+		  // Called when there's incoming data on the websocket for this channel
+		}
+	  });
+})
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
-
-  received(data) {
-    // Called when there's incoming data on the websocket for this channel
-  }
-});
