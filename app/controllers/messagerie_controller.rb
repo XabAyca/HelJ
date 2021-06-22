d6b8d9372af2ee6f2eb39ab340714c3706eb40aa
jocelin queau <queau.j@gmail.com>
2021-06-22 11:16:41 +0200

    messagerie not real time working

--- app/controllers/messagerie_controller.rb
+++ app/controllers/messagerie_controller.rb
@@ -1,12 +1,21 @@
 class MessagerieController < ApplicationController
   def index
-    @rooms = User.find(current_user.id).rooms
+    if user_signed_in?
+      @rooms = User.find(current_user.id).rooms
+    else
+      redirect_to root_path
+    end
   end
 
   def show
     p params[:id]
     @rooms = User.find(current_user.id).rooms
-	@room = Room.find(params[:id])
+    @room = Room.find(params[:id])
+    p "i" * 30
+    p @room
+    p @room.messages
+    p @room.messages[0]
+    p @room.messages[0].content
     render "index"
   end
 
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,8 @@
 class MessagesController < ApplicationController
   def create
-    p params
-    @message = Message.create(content: params[:content], room_id: params[:room_id], user_id: current_user.id)
+    @message = Message.new(content: params[:content], room_id: params[:room_id], user_id: current_user.id)
+    if @message.save
+      redirect_to request.referrer
+    end
   end
 end
