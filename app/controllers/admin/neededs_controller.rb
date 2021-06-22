014bb79f7d46a4bd54fded0ed73d326e005040ae
Xabi <xabi.aycaguer@gmail.com>
2021-06-22 09:19:22 +0200

    add needed view & controller to admin dashboard

--- app/controllers/admin/neededs_controller.rb
+++ app/controllers/admin/neededs_controller.rb
@@ -1,4 +1,37 @@
 class Admin::NeededsController < ApplicationController
   before_action :authenticate_user!
   before_action :is_admin?
+
+  def index
+    @neededs = Needed.all
+  end
+
+  def new
+    @needed = Needed.new
+  end
+
+  def create
+    @needed = Needed.new(post_params)
+    if @needed.save
+      flash[:success] = 'Besoins créé'
+      redirect_to admin_neededs_path
+    else
+      flash[:error] = 'Besoins non créé'
+      render 'new'
+    end
+  end
+
+  def destroy
+    @needed = Needed.find(params[:id])
+    @needed.destroy
+    flash[:success] = 'Besoins supprimé'
+    redirect_to admin_neededs_path
+  end
+
+  private
+
+  def post_params
+    params.require(:needed).permit(:name, :description)
+  end
+
 end
