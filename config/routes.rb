dba99cf616a0007ff288223afc7fe7fe33a9c8ac
jocelin queau <queau.j@gmail.com>
2021-06-23 02:28:35 +0200

    formating config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
   root "static_pages#index"
-  get '/resources', to:'static_pages#resources'
+  get "/resources", to: "static_pages#resources"
 
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', :registrations => "registrations" }
- 
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks", :registrations => "registrations" }
+
   resources :projects
   resources :challenges, only: [:index, :show]
-  resources :joint_users_to_projects, only: [:create,:destroy]
-  get '/joint_the_challenge/:id_user/:id_challenge', to: 'joint_the_challenge#index', as: 'joint_the_challenge'
+  resources :joint_users_to_projects, only: [:create, :destroy]
+  get "/joint_the_challenge/:id_user/:id_challenge", to: "joint_the_challenge#index", as: "joint_the_challenge"
 
   namespace :admin do
-    root 'static_pages#index'
+    root "static_pages#index"
     resources :projects, except: [:show]
-    resources :users, only: [:index,:destroy,:update]
-    resources :challenges, except: [:update,:edit,:show]
-    resources :neededs, except: [:update,:edit,:show]
+    resources :users, only: [:index, :destroy, :update]
+    resources :challenges, except: [:update, :edit, :show]
+    resources :neededs, except: [:update, :edit, :show]
   end
 end
