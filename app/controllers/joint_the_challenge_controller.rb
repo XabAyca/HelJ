7ffb81e1a7610cafab7fdb963b11e52e03f46963
Lehoax <leolair.pro@gmail.com>
2021-06-23 07:23:09 -0700

    redirect challenge

--- app/controllers/joint_the_challenge_controller.rb
+++ app/controllers/joint_the_challenge_controller.rb
@@ -5,6 +5,8 @@ class JointTheChallengeController < ApplicationController
         JointUsersToPpation.create(user_id: @user.id, participation_challenge_id: @challenge.id)
         ids = [@user.id , @challenge.id]
         ApplicationMailer.joint_the_challenge(ids).deliver
+    redirect_to challenge_path(@challenge.id),notice:'Vous êtes inscrit sur le challenge, un email vous a été envoyé'
+
     end
 
     
