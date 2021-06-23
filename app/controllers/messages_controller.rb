38306d4462e08ecb70c2cf8ca8953ea046dfaaab
jocelin queau <queau.j@gmail.com>
2021-06-23 15:11:25 +0200

    messaging almost over

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,9 @@ class MessagesController < ApplicationController
   def create
     @message = Message.new(content: params[:content], room_id: params[:room_id], user_id: current_user.id)
     if @message.save
+      p @message
+      p @message.user.id
+      p current_user.id
       ActionCable.server.broadcast "room_channel_#{@message.room_id}", html: render_message
     end
   end
