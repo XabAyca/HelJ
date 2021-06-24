77171c6851b670874a5ded06302e287c44e32752
Xabi <xabi.aycaguer@gmail.com>
2021-06-24 21:29:24 +0200

    update join challenge

--- app/controllers/joint_the_challenge_controller.rb
+++ app/controllers/joint_the_challenge_controller.rb
@@ -1,9 +1,9 @@
 class JointTheChallengeController < ApplicationController
-    def index
-        @user = User.find(params['id_user'])
-        @challenge = Challenge.find(params['id_challenge'])
-        JointUsersToPpation.create(user_id: @user.id, participation_challenge_id: @challenge.id)
-    end
-
     
+  def index
+    @user = User.find(params['id_user'])
+    @challenge = Challenge.find(params['id_challenge'])
+    JointUsersToPpation.create(user_id: @user.id, participation_challenge_id: @challenge.id)
+    redirect_to challenge_path(@challenge.id), notice: 'Vous êtes bien inscrit sur le challenge'
+  end
 end
