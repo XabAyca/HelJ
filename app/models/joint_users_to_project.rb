d9906d26c0d45545574070d7f5d2b571cd2111dc
Lehoax <leolair.pro@gmail.com>
2021-06-24 07:05:37 -0700

    fix load font

--- app/models/joint_users_to_project.rb
+++ app/models/joint_users_to_project.rb
@@ -1,8 +1,4 @@
 class JointUsersToProject < ApplicationRecord
   belongs_to :user
   belongs_to :project
-  after_create :mail_joint_project
-  def mail_joint_project
-    UserMailer.joint_project(current_user, params[:project_id]).deliver
-  end
 end
