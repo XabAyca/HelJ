db2f3ff260acafaf262a57914874c5ee885537c2
jocelin queau <queau.j@gmail.com>
2021-06-25 11:41:27 +0200

    resolving conflict in challenge

--- app/controllers/joint_the_challenge_controller.rb
+++ app/controllers/joint_the_challenge_controller.rb
@@ -1,9 +1,16 @@
 class JointTheChallengeController < ApplicationController
-    
   def index
-    @user = User.find(params['id_user'])
-    @challenge = Challenge.find(params['id_challenge'])
-    JointUsersToPpation.create(user_id: @user.id, participation_challenge_id: @challenge.id)
-    redirect_to challenge_path(@challenge.id), notice: 'Vous êtes bien inscrit sur le challenge'
+    @user = User.find(params["id_user"])
+    @challenge = Challenge.find(params["id_challenge"])
+    pc = ParticipationChallenge.new(challenge_id: @challenge.id)
+    p "*" * 100
+    p pc, pc.valid?
+    pc.save
+    jtp = JointUsersToPpation.new(user_id: @user.id, participation_challenge_id: pc.id)
+    p jtp
+    p jtp.valid?
+    jtp.save
+    p "*" * 100
+    redirect_to challenge_path(@challenge.id), notice: "Vous êtes bien inscrit sur le challenge"
   end
 end
--- app/models/joint_users_to_ppation.rb
+++ app/models/joint_users_to_ppation.rb
@@ -3,7 +3,7 @@ class JointUsersToPpation < ApplicationRecord
   belongs_to :participation_challenge, optional: true
   after_create :user_joint_challenge
 
-    def user_joint_challenge
-      UserMailer.user_on_challenge(self.user_id, self.participation_challenge_id).deliver
-    end
+  def user_joint_challenge
+    UserMailer.user_on_challenge(self.user_id, self.participation_challenge.challenge.id).deliver
+  end
 end
